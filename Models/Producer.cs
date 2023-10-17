95d6bcf27dd83e367d20116f6e48a0996a66e954
christianhall058 <christianhall058@gmail.com>
2023-10-17 11:20:54 -0500

    Added the producer model class

---
+++ Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcomerceApp.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProfilePictureURL { get; set; }
+
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
