608253cb5cc603e14f0239b746d2e781029146ff
christianhall058 <christianhall058@gmail.com>
2023-10-17 10:54:09 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcomerceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
