f3a1358a88e816705346c702633256b35ed9ac2e
christianhall058 <christianhall058@gmail.com>
2023-10-17 11:17:48 -0500

    Added actor model class

---
+++ Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcomerceApp.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProfilePictureURL { get; set; }
+
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
