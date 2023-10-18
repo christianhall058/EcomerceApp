d14c0487010f51b4085f3274237becfb8eac5792
christianhall058 <christianhall058@gmail.com>
2023-10-18 13:06:34 -0500

    Added the Actors controller

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using EcomerceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcomerceApp.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
