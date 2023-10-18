5b7e2ab1f91f0c4ebb1b8bc16824fd29490a9525
christianhall058 <christianhall058@gmail.com>
2023-10-18 13:12:08 -0500

    Added the producer controller. Used the async way to grab the view and producers

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using EcomerceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcomerceApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
