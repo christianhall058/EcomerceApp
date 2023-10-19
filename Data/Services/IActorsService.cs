dd8270632345a11bd28847faa1d62b02b0fd2e58
christianhall058 <christianhall058@gmail.com>
2023-10-19 12:41:42 -0500

    Created a navbar and changed the home view to the movies view. Added service class for actors

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using EcomerceApp.Models;
+
+namespace EcomerceApp.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
